b4f7e548a9309c595ffdee7e1fed14fc4a545945
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-09-04 22:27:02 -0300

    criado o projeto   e iniciado o serviço.

---
+++ EtecShopAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+using MongoDB.Driver;
+
+namespace EtecShopAPI.Extensions;
+
+    public static class ServiceExtensions 
+    {
+        public static void ConfigureCors(this IServiceCollection services) 
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader() 
+                );
+            });
+
+        }
+
+        public static void ConfigureMongoDBSettings( this IServiceCollection services, IConfiguration config)
+        {
+
+        }
+    }
